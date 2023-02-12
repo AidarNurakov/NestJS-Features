d61f32465313c4f9323deed40b2e94a7c8e54849
AidarNurakov <aidar11221998@gmail.com>
2023-02-12 21:40:55 +0600

    feature/users creation and findOne

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import CreateUserDto from './dto/createUser.dto';
+import User from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User) private readonly usersRepository: Repository<User>) {}
+
+    async getByEmail(email: string) {
+        const user = await this.usersRepository.findOneBy({ email })
+        if(user) {
+            return user;
+        }
+        throw new HttpException('User with this email does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    async create(userData: CreateUserDto) {
+        const newUser = this.usersRepository.create(userData);
+        await this.usersRepository.save(newUser);
+        return newUser;
+    } 
+}
