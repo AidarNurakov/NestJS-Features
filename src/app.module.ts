cf92721d2b7e63560980e2995fb6e3dc3119a6b9
AidarNurakov <aidar11221998@gmail.com>
2023-02-12 21:31:07 +0600

    feature/users module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { PostsModule } from './posts/posts.module';
 import { ConfigModule } from "@nestjs/config";
 import * as Joi from '@hapi/joi';
 import { DatabaseModule } from "./database/database.module";
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -18,7 +19,8 @@ import { DatabaseModule } from "./database/database.module";
       })
     }),
     DatabaseModule,
-    PostsModule
+    PostsModule,
+    UsersModule
   ],
   controllers: [],
   providers: [],
